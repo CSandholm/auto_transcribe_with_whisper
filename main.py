b1184595b24f48a1ff58dab306e5afd547ef58de
CharlesQuicksearch <charles.sandholm@quicksearch.se>
2023-09-28 15:05:40 +0200

    added logging and configs

--- main.py
+++ main.py
@@ -1,11 +1,32 @@
-import time
-from procedures import transcribe_audio
+import schedule
 
-path = "C:/Users/CharlesSandholm/Documents/Charles Sandholm/Jupyter/S2TApplication/testfolder"
+from procedures import transcribe_audio
+from Logger import logger
 
 def main():
+
+    config = {}
+    with open('config.txt', 'r') as f:
+        for line in f:
+            key, value = line.strip().split(' = ')
+            config[key] = value
+
+    path = config.get("path_to_customer_folders")
+
+    #Call logger configuration
+    logger.config_logger()
+
+    transcribe_audio(path)
+
+    #Check for audio files in path directory every 5 minutes.
+    schedule.every(5).minutes.do(transcribe_audio, path)
+
+    #Start writing logs in a new logging file
+    schedule.every().day.at("00:00").do(logger.config_logger)
+
     while True:
-        transcribe_audio(path)
-        time.sleep(300)
+        #As long as schedules are pending the application won't stop running
+        schedule.run_pending()
 
 main()
+
--- procedures.py
+++ procedures.py
@@ -1,8 +1,10 @@
 import whisper
 import os
 from os import walk
+from Logger.logger import logging
 
 model = whisper.load_model("tiny")
+
 valid_audio_sources = ["mp3", "mp4", "mpeg", "mpga", "m4a", "wav", "webm"]
 
 def transcribe_audio(path):
@@ -10,17 +12,26 @@ def transcribe_audio(path):
         if 'directory' in dirs:
             dirs.remove('directory')
         if '.ipynb_checkpoints' in dirs:
-            dirs.remove('.ipynb_checkpoints')
+            dirs.remove('.ipynb_checkpoints') #Exclusive for test in jupyter notebook/lab
         if 'archive' in dirs:
             dirs.remove('archive')
         for filename in files:
             if any(filename.endswith(ext) for ext in valid_audio_sources):
+                current_dir = os.getcwd()
+                logging.info(f"Found file {filename} in {current_dir}")
                 file_path = os.path.join(root, filename)
-                result = model.transcribe(file_path, language="sv", fp16=False, verbose=True)
-                new_file_name = "/transcribed_" + filename.rsplit(".",1)[0]
+                try:
+                    result = model.transcribe(file_path, language="sv", fp16=False, verbose=True)
+                    logging.info(f"{filename} transcribed.")
+                    new_file_name = "/transcribed_" + filename.rsplit(".",1)[0]
 
-                with open(root+new_file_name + ".txt", "w") as f:
-                    f.write(result["text"])
-                    f.close()
+                    with open(root+new_file_name + ".txt", "w") as f:
+                        f.write(result["text"])
+                        f.close()
 
-                os.rename(file_path, root+"/archive/"+filename)
+                    os.rename(file_path, root+"/archive/"+filename)
+                except Exception as e:
+                    logging.error(f"{e}")
+
+
+
